b6b93716d84251e6b69eba98c70f17f592576ab0
Giffari Alfarizy <giffarialfarizy@gmail.com>
2022-02-26 18:01:17 +0700

    ➕ Add gh-pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	assetPrefix: isProd
+		? '/https://github.com/giffarialfarizy/giffarialfarizy.github.io/'
+		: '',
+};
